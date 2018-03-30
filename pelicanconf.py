e5671df1d995a525e3ff545f96e6c039f0f1fe30
Adam Parkin <adam.parkin@bambora.com>
2018-03-29 22:57:36 -0700

    Add social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,18 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Github', 'https://github.com/pzelnip'),
+#          ('Twitter', 'https://twitter.com/codependentcodr'),
+#          ('StackOverflow', 'http://stackoverflow.com/users/808804'),
+#          ('LinkedIn', 'http://lnkd.in/ykHQiG'),
+#          )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/pzelnip'),
+          ('Twitter', 'https://twitter.com/codependentcodr'),
+          ('StackOverflow', 'http://stackoverflow.com/users/808804'),
+          ('LinkedIn', 'http://lnkd.in/ykHQiG'),
+          )
 
 DEFAULT_PAGINATION = 10
 
