2dff915adad1cb42738e2fa6082b98b808c2515a
Adam Parkin <adam.parkin@bambora.com>
2018-03-30 06:30:07 -0700

    Fix dev config site url

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Adam Parkin'
 SITENAME = 'The Codependent Codr'
-SITEURL = 'http://www.codependentcodr.com'
+SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
 
