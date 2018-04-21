efb889aaf5e42b1340d43ab214d9ceb42341f647
Adam Parkin <adam.parkin@bambora.com>
2018-04-21 14:51:07 -0700

    Make sitedir

--- site_look_monitor/test.sh
+++ site_look_monitor/test.sh
@@ -4,5 +4,7 @@ set -e
 AWS_SECRET=$(aws --profile codependentcodr configure get aws_secret_access_key)
 AWS_KEY=$(aws --profile codependentcodr configure get aws_access_key_id)
 
+mkdir -p sitecapt
+
 docker build -t sitelookmonitor -f Dockerfile .
 docker run -e AWS_ACCESS_KEY_ID=$AWS_KEY -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET --rm -v `pwd`/sitecapt:/build sitelookmonitor:latest
