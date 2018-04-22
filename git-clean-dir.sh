6c7394f60c0a5fac7f5e3389d476474ed87c6c66
Adam Parkin <adam.parkin@bambora.com>
2018-04-21 17:02:27 -0700

    Add branch check to git-clean-dir

--- git-clean-dir.sh
+++ git-clean-dir.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$BRANCH" != "master" ]]; then
+    echo "Not on master, currently on $BRANCH"
+    exit 1
+fi
 
 if [ -z "$(git status --porcelain)" ]; then
     exit 0
