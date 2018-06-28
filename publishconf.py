94992cd85291927d8e7429a550f5268a52909cbd
Adam Parkin <adam.parkin@bambora.com>
2018-06-27 19:31:16 -0700

    Blacken the python source [ch224]

--- publishconf.py
+++ publishconf.py
@@ -8,23 +8,25 @@
 from __future__ import unicode_literals
 import os
 import sys
+
 sys.path.append(os.curdir)
 # pylint: disable=wildcard-import,wrong-import-position
 from pelicanconf import *
+
 # pylint: enable=wildcard-import,wrong-import-position
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-SITEURL = 'https://www.codependentcodr.com'
+SITEURL = "https://www.codependentcodr.com"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# DISQUS_SITENAME = ""
+# GOOGLE_ANALYTICS = ""
