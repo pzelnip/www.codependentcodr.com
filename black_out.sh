344fa849aa08e0266cc028a4ecba06dec5c304f0
Adam Parkin <pzelnip@gmail.com>
2018-08-06 13:17:23 -0700

    dfslkjafcs

--- black_out.sh
+++ black_out.sh
@@ -10,14 +10,15 @@ if [ -z "$(git status --porcelain)" ]; then
     exit 0
 else
     echo "Dirty directory"
-    git branch
-    ls
     git --no-pager log -n 20
+    echo "Checking out $2"
+    git checkout $2
+    git branch
     git commit -am "BLACK-123 Automated Black out"
     echo "Committed now what"
     git --no-pager log -n 20
     git remote -v
-    # git push https://$1@github.com/pzelnip/www.codependentcodr.com $2
+    git push https://$1@github.com/pzelnip/www.codependentcodr.com $2
     echo "Blackened changes, aborting build"
     exit 1
 
