6b2d675fa373e1f6199743b6613012091523f44b
Adam Parkin <pzelnip@gmail.com>
2018-08-06 12:41:19 -0700

    Use vars passed to black_out.sh script [ch233]

--- black_out.sh
+++ black_out.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Blackening code for $TRAVIS_BRANCH...."
+echo "Blackening code for $2...."
 
 docker run -it --rm -v `pwd`:/build codependentcodr:latest black .
 
@@ -10,8 +10,9 @@ if [ -z "$(git status --porcelain)" ]; then
     exit 0
 else
     echo "Dirty directory"
+    git branch
     git commit -am "BLACK-123 Automated Black out"
-    git push https://${GH_TOKEN}@github.com/pzelnip/www.codependentcodr.com $TRAVIS_BRANCH
+    git push https://$1@github.com/pzelnip/www.codependentcodr.com $2
     echo "Blackened changes, aborting build"
     exit 1
 fi
